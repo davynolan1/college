44f574e84b64d1ac82ed23bef04a09f6e8d3f70a
Davy Nolan <dnolan5@tcd.ie>
2019-10-06 19:44:09 +0100

    Computer arch tutorial 2

---
+++ year3/computer_architecture_2/t2Test/t2Test/t2Test/t2.h
@@ -0,0 +1,6 @@
+extern "C" _int64 min(_int64, _int64, _int64);
+extern "C" _int64 p(_int64, _int64, _int64, _int64);
+extern "C" _int64 gcd(_int64, _int64);
+extern "C" _int64 q(_int64, _int64, _int64, _int64, _int64);
+extern "C" _int64 qns();
+extern "C" _int64 g; 
